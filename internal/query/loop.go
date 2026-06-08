5a16a6ed6a89e4a993daa5c5cdcf641377d90d40
Parth Mudgal <artpar@gmail.com>
2026-06-08 09:17:26 +0530

    Fix pragma bash loop final answers

--- internal/query/loop.go
+++ internal/query/loop.go
@@ -109,12 +109,7 @@ func (e *Engine) seedFromRequest(params provider.RequestParams) {
 func (e *Engine) runLoop(ctx context.Context, userMessage string, ch chan<- LoopEvent) {
 	observe.TraceCtx(ctx, "query", "Engine.runLoop", "enter")
 	defer observe.TraceCtx(ctx, "query", "Engine.runLoop", "exit")
-	if os.Getenv("PRAGMA_LEGACY_TOOL_LOOP") != "1" {
-		e.runPragmaLoop(ctx, userMessage, ch)
-		return
-	}
-
-	e.runLegacyToolLoop(ctx, &userMessage, ch)
+	e.runPragmaLoop(ctx, userMessage, ch)
 }
 
 func (e *Engine) runLegacyToolLoop(ctx context.Context, initialUserMessage *string, ch chan<- LoopEvent) {
